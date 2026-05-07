d3ff56d996e69c2270a2071b754b5ffb3d265542
bhanubais <bhanubais@gmail.com>
2026-05-07 11:28:53 +0530

    Starry Night completed

--- 2_Starry_Night/index.js
+++ 2_Starry_Night/index.js
@@ -1,8 +1,23 @@
+import { randomRange, range } from "../libs/utilities.js";
+
 const btn = document.querySelector(".particleButton");
 
-btn.addEventListener("click", () => {
+btn.addEventListener("click", (e) => {
 	// TODO: Generate stars on click!
 	// Here’s the emoji to use: ⭐
+
+	for (let i = 0; i < 10; i++) {
+		const star = document.createElement("span");
+		const star_wrapper = document.querySelector(".stars_wrapper");
+
+		star.innerText = "⭐";
+		star.classList.add("star");
+		star.style.top = `${randomRange(0, 100)}%`;
+		star.style.left = `${randomRange(0, 100)}%`;
+
+		// Add star to the DOM
+		star_wrapper.appendChild(star);
+	}
 });
 
 /*
