862b4459d21d05de6dc33830eb14afb4255fd374
bhanubais <bhanubais@gmail.com>
2026-05-07 09:57:10 +0530

    Particle Cleanup

--- 1_Particles/index.js
+++ 1_Particles/index.js
@@ -3,6 +3,9 @@ import { randomRange, range } from "./libs/utilities.js";
 const btn = document.querySelector(".particleButton");
 
 btn.addEventListener("click", (e) => {
+	const FADE_DURATION = 1000;
+	const bubbles = [];
+
 	const element = e.currentTarget;
 	element.classList.toggle("liked");
 	const isLiked = element.classList.contains("liked");
@@ -16,6 +19,15 @@ btn.addEventListener("click", (e) => {
 		bubble.classList.add("particle");
 		bubble.style.top = `${randomRange(0, 100)}%`;
 		bubble.style.left = `${randomRange(0, 100)}%`;
+		bubble.style.animationDuration = `${FADE_DURATION}ms`;
 		element.appendChild(bubble);
+
+		bubbles.push(bubble);
 	});
+
+	window.setTimeout(() => {
+		bubbles.forEach((bubble) => {
+			bubble.remove();
+		});
+	}, FADE_DURATION + 100);
 });
