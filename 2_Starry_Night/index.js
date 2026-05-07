726d47b4624c6f805ff50f19ab23952d74e1e9c1
bhanubais <bhanubais@gmail.com>
2026-05-07 10:33:02 +0530

    Starry Night task setup

---
+++ 2_Starry_Night/index.js
@@ -0,0 +1,21 @@
+const btn = document.querySelector(".particleButton");
+
+btn.addEventListener("click", () => {
+	// TODO: Generate stars on click!
+	// Here’s the emoji to use: ⭐
+});
+
+/*
+  DOM manipulation cheatsheet:
+
+  - Select an element:
+    `document.querySelector('.someCssSelector');`
+  - Create new elements:
+    `document.createElement('tagName');`
+  - Add a CSS class:
+    `element.classList.add('className');`
+  - Add the element to the DOM:
+    `parentNode.appendChild(childNode);`
+  - Add text to a node:
+    `element.innerText = "Stuff";`
+*/
