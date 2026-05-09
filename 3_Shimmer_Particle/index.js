be0e2894d56939f835b4d13670b4465a6b30e23c
bhanubais <bhanubais@gmail.com>
2026-05-09 12:54:09 +0530

    Shimmer Particle implemented

--- 3_Shimmer_Particle/index.js
+++ 3_Shimmer_Particle/index.js
@@ -1,10 +1,16 @@
 const btn = document.querySelector(".buyButton");
 
 function generateShimmer() {
+	const SHIMMER_DURATION = 1000;
+
 	const shimmer = document.createElement("span");
 	shimmer.classList.add("shimmer");
+	shimmer.style.animationDuration = `${SHIMMER_DURATION}ms`;
 
 	btn.appendChild(shimmer);
+	window.setTimeout(() => {
+		shimmer.remove();
+	}, SHIMMER_DURATION);
 }
 
 btn.addEventListener("mouseenter", generateShimmer);
